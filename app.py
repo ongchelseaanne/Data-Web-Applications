21656be45e2e269f90e168b27474cb2a4a7d470f
Chelsea Anne Ong <csong@aim.edu>
2020-05-28 17:42:08 +0800

    updated code

--- app.py
+++ app.py
@@ -137,5 +137,7 @@ def upload():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8082,debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='127.0.0.1', port=8082,debug=True)
 
